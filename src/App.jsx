90c0bade995cd2514f9df2abae90dd8016a24619
victor.sh.3004@gmail.com <victor.sh.3004@gmail.com>
2024-02-20 19:05:33 -0500

    menu Header and title content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 import Sidebar from "./components/shared/Sidebar";
-import {RiMenu3Fill, RiAddLine, RiPieChartLine, RiLightbulbLine} from "react-icons/ri";
+import {RiMenu3Fill, RiAddLine, RiPieChartLine, RiLightbulbLine, RiCloseLine, RiSearch2Line, RiArrowDownSLine} from "react-icons/ri";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  const toggleMenu = () =>{
+    setShowMenu(!showMenu);
+  }
   return <div className="bg-[#262837] w-full min-h-screen">
-    <Sidebar />
+    <Sidebar showMenu={showMenu}/>
     {/* Menu movil */}
     <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-600 p-4 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
       <button>
@@ -15,11 +23,51 @@ function App() {
       <button>
         <RiPieChartLine/>
       </button>
-      <button className="text-white">
-        <RiMenu3Fill/>
+      <button onClick={toggleMenu} className="text-white p-2">
+        {showMenu ? <RiCloseLine className="text-[#ec7c6a]"/> : <RiMenu3Fill/>}
       </button>
     </nav>
-
+    <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8 p-4">
+      <div className="lg:col-span-6">
+        {/*Header */}
+        <header>
+          {/*Title and search */}
+          <div className="flex flex-col md:flex-row md:justify-between gap-4 mb-6">
+            <div>
+              <h1 className="text-gray-200">Jeager Resto</h1>
+              <p className="text-gray-500">20 febrero 2024</p>
+            </div>
+            <form action="">
+              <div className="w-full relative">
+                <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300"/>
+                <input type="text" className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none " placeholder="search" />
+              </div>
+            </form>
+          </div>
+          {/*Tabs */}
+          <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
+            <a 
+              href="#" 
+              className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:lef-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]"
+            >
+              Hot dishes
+            </a>
+            <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:lef-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]">Cold dishes</a>
+            <a href="#" className="py-2 pr-4">Soup</a>
+            <a href="#" className="py-2 pr-4">Gril</a>
+          </nav>
+        </header>
+        {/*Title content */}
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+          <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg"><RiArrowDownSLine/>
+          Dine in
+          </button>
+        </div>
+        {/*Content */}
+      </div>
+      <div className="lg:col-span-2 bg-red-500 fixed lg:static right-0">carrito</div>
+    </main>
     </div>;
 }
 
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import {RiHome6Line,RiPercentLine,RiPieChartLine,RiMailLine,RiNotification3Line,RiSettings4Line,RiLogoutCircleRLine} from "react-icons/ri";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { showMenu } = props;
   return (
-    <div className='bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full'>
+    <div className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 ${showMenu ? "left-0" : "-left-full"}`}>
       <div>
         <ul className="pl-4">
             <li>
